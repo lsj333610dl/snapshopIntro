066a6f965e4f48359ea902ed6f241bbecc938880
lsj333610dl <lsj333610dl@naver.com>
2014-12-17 17:00:37 +0900

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+	    navigation: true,
+	    navigationPosition: 'right',
+		navigationTooltips: ['Indroduction', 'iOS App', 'Android App', 'Server'],
+		sectionsColor: ['#c74f4f','#4ec598','#4c6dc0','#4199a3']
+    });
+});
